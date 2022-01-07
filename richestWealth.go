0b8fbf1720c749808cad2f34019c8c933d804757
ArjunMalhotra07 <arjun03.malhotra@gmail.com>
2022-01-08 02:42:43 +0530

    Richest Wealth Ques

---
+++ richestWealth.go
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/richest-customer-wealth/submissions/
+
+func maximumWealth(accounts [][]int) int {
+
+	ans := 0
+	for i := 0; i < len(accounts); i++ {
+		sum := 0
+		for j := 0; j < len(accounts[i]); j++ {
+			sum = sum + accounts[i][j]
+		}
+		if sum > ans {
+			ans = sum
+		}
+
+	}
+	return ans
+
+}
