40bc73fa49eec4caa2855ea955774b9643f24a72
ArjunMalhotra07 <arjun03.malhotra@gmail.com>
2022-01-17 21:54:18 +0530

    Missing Number in a Sorted Array

---
+++ Cyclic_Sort/06_MissingNumber.go
@@ -0,0 +1,42 @@
+//Q268..    https://leetcode.com/problems/missing-number/
+
+package main
+
+import "fmt"
+
+func main() {
+	var nums = []int{0, 2, 3, 4, 5, 6, 7, 7}
+	ans := SelectionSort(nums)
+	fmt.Println(nums)
+	fmt.Println(ans)
+}
+
+func SelectionSort(nums []int) int {
+
+	i := 0
+	for i < len(nums) {
+		correct := nums[i]
+		if nums[i] < len(nums) && nums[i] != nums[correct] { //N se chota rhe hmara i
+			swap(nums, i, correct)
+
+		} else {
+			i++
+		}
+	}
+
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != i {
+			return i
+			//return nums[i]
+		}
+
+	}
+	return len(nums)
+
+}
+func swap(nums []int, i int, correct int) {
+
+	temp := nums[correct]
+	nums[correct] = nums[i]
+	nums[i] = temp
+}
