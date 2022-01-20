10976cf45f5a5afff94986511495d446b4bb9370
ArjunMalhotra07 <arjun03.malhotra@gmail.com>
2022-01-20 23:48:28 +0530

    If a sentence is palindrome or not

---
+++ Strings/07ValidPalindrome.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"regexp"
+	"strings"
+)
+
+func main() {
+	p := fmt.Println
+	p(checkAns("A man, a plan, a canal: Panama"))
+}
+
+func checkAns(test string) bool {
+	p := fmt.Println
+	testString := strings.ToLower(test)
+
+	reg, err := regexp.Compile("[^a-zA-Z0-9]+")
+	if err != nil {
+		log.Fatal(err)
+	}
+	processedString := reg.ReplaceAllString(testString, "")
+	p(processedString)
+	ans := booleanAns(processedString)
+	return ans
+}
+
+func booleanAns(processedString string) bool {
+	byte_String := []byte(processedString)
+	var temp byte
+
+	for i, j := 0, len(byte_String)-1; i < j; i, j = i+1, j-1 {
+		temp = byte_String[i]
+		byte_String[i] = byte_String[j]
+		byte_String[j] = temp
+	}
+
+	if string(byte_String) == processedString {
+		return true
+
+	} else {
+		return false
+	}
+}
