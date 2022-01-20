6c72dc218d5925f46ca3c7add60107b4d89da22c
ArjunMalhotra07 <arjun03.malhotra@gmail.com>
2022-01-21 03:33:49 +0530

    Length of Last Word

--- Strings/08LenOfLastWord.go
+++ Strings/08LenOfLastWord.go
@@ -13,12 +13,17 @@ func main() {
 
 }
 
+// func lengthOfLastWord(s string) int {
+// 	x := fmt.Println
+// 	words := strings.Fields(s)
+// 	for i, j := 0, len(words)-1; i < j; i, j = i+1, j-1 {
+// 		words[i], words[j] = words[j], words[i]
+// 	}
+// 	x(strings.Join(words, " "))
+// 	return len(words[0])
+// }
+
 func lengthOfLastWord(s string) int {
-	x := fmt.Println
 	words := strings.Fields(s)
-	for i, j := 0, len(words)-1; i < j; i, j = i+1, j-1 {
-		words[i], words[j] = words[j], words[i]
-	}
-	x(strings.Join(words, " "))
-	return len(words[0])
+	return len(words[len(words)-1])
 }
