48ea6533ee11641b6ceebde4bc249ee941957d13
ArjunMalhotra07 <arjun03.malhotra@gmail.com>
2022-01-31 23:41:45 +0530

    Return Middle Of Linked List

---
+++ linkedList/06middleNode.go
@@ -0,0 +1,37 @@
+//Return middle Node
+func middleNode(head *ListNode) *ListNode {
+	currentNode := head
+	length := 0
+	for currentNode != nil {
+		length++
+		currentNode = currentNode.Next
+	}
+	if length == 1 {
+		return head
+	}
+	mid := length / 2
+	currentNode1 := head
+	temp := 0
+	for temp != mid {
+		temp++
+		currentNode1 = currentNode1.Next
+	}
+	return currentNode1
+}
+
+//Or
+func middleNode(head *ListNode) *ListNode {
+	slow := head
+	fast := head
+	if head.Next == nil {
+		return head
+	}
+	for fast != nil || fast.Next != nil {
+		fast = fast.Next.Next
+		if fast == nil || fast.Next == nil {
+			return slow.Next
+		}
+		slow = slow.Next
+	}
+	return nil
+}
