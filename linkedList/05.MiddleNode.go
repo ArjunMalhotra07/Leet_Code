42a947ca8dee303b1010ec7f71e38a91bba0f8b7
ArjunMalhotra07 <arjun03.malhotra@gmail.com>
2022-01-31 23:40:40 +0530

    Delete Nth Node

--- linkedList/05.MiddleNode.go
+++ linkedList/05.MiddleNode.go
@@ -1,54 +1,44 @@
-/**
- * Definition for singly-linked list.
- * type ListNode struct {
- *     Val int
- *     Next *ListNode
- * }
- */
+//Delete Middle of the Linked List
 func deleteMiddle(head *ListNode) *ListNode {
-    currentNode:=head
-    length:=0
-    for currentNode!=nil{
-        length++
-        currentNode=currentNode.Next
-    }
-    if length==1{
-        return nil
-    }
-    mid:= length/2
-    previousNode:=head
-    temp:=0
-    for  previousNode!=nil{
-        temp+=1
-        if temp==mid{
-      previousNode.Next=previousNode.Next.Next
-      return head
-        }
-       previousNode=previousNode.Next
-    }
-    return nil
+	currentNode := head
+	length := 0
+	for currentNode != nil {
+		length++
+		currentNode = currentNode.Next
+	}
+	if length == 1 {
+		return nil
+	}
+	mid := length / 2
+	previousNode := head
+	temp := 0
+	for previousNode != nil {
+		temp += 1
+		if temp == mid {
+			previousNode.Next = previousNode.Next.Next
+			return head
+		}
+		previousNode = previousNode.Next
+	}
+	return nil
 }
 
-
-
-{
-OR
+//OR
 func deleteMiddle(head *ListNode) *ListNode {
-    
-    slow:=head
-    fast:=head
-    if head.Next==nil{
-        return nil
-    }
-    for fast!=nil || fast.Next!=nil{
-        fast=fast.Next.Next
-        if fast==nil || fast.Next==nil{
-            slow.Next=slow.Next.Next
-            return head
-        }
-        slow=slow.Next
-    }
-    
-    return nil
-}
+
+	slow := head
+	fast := head
+	if head.Next == nil {
+		return nil
+	}
+	for fast != nil || fast.Next != nil {
+		fast = fast.Next.Next
+		if fast == nil || fast.Next == nil {
+			slow.Next = slow.Next.Next
+			return head
+		}
+		slow = slow.Next
+	}
+
+	return nil
 }
