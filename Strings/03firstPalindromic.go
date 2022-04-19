5ea4128bb210efccefd905bd2daf0f2d02323b6c
ArjunMalhotra07 <arjun03.malhotra@gmail.com>
2022-04-19 16:34:39 +0530

    Changed bits and pieces

--- Strings/03firstPalindromic.go
+++ Strings/03firstPalindromic.go
@@ -3,13 +3,19 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
 
 	p := fmt.Println
-	words := []string{"racecar", "car", "ada", "racecar", "cool"}
+	x := "Hello my name is arjun"
+	p(strings.Fields(x))
+
+	words := []string{"cooc", "car", "ada", "racecar", "cool"}
 	p(firstPalindrome(words))
+	p(check(x))
+	p(palindrome(x))
 
 }
 
@@ -22,20 +28,60 @@ func firstPalindrome(words []string) string {
 	}
 	return ""
 }
+
+// func check(ans string) bool {
+// 	test := ans
+// 	test_byte := []byte(test)
+// 	var temp1 byte
+// 	var temp2 byte
+// 	//1 by 1 krlia check
+// 	for i, j := 0, len(test_byte)-1; i < j; i, j = i+1, j-1 {
+// 		temp1 = test_byte[i]
+// 		temp2 = test_byte[j]
+
+// 		if temp1 == temp2 {
+// 			return true
+// 		} else {
+// 			return false
+// 		}
+
+// 	}
+// 	return false
+// }
+
 func check(ans string) bool {
 	test := ans
 	test_byte := []byte(test)
-	var temp1 byte
-	var temp2 byte
-	//Ye krlia check 1 by 1 OR Reverse the word and then check
-	for i, j := 0, len(test_byte)-1; i < j; i, j = i+1, j-1 {
-		temp1 = test_byte[i]
-		temp2 = test_byte[j]
 
-		if temp1 == temp2 {
+	//1 by 1 krlia check
+	for i, j := 0, len(test_byte)-1; i < j; i, j = i+1, j-1 {
+		s1 := string(test_byte[i])
+		s2 := string(test_byte[j])
+
+		if s1 == s2 {
 			return true
+		} else {
+			return false
 		}
 
 	}
 	return false
+}
+
+func palindrome(ans string) string {
+	test := ans
+	test_byte := []byte(test)
+
+	//1 by 1 krlia check
+	for i, j := 0, len(test_byte)-1; i < j; i, j = i+1, j-1 {
+		s1 := string(test_byte[i])
+		s2 := string(test_byte[j])
+
+		var temp string
+		temp = s1
+		s1 = s2
+		s2 = temp
+
+	}
+	return string(test_byte)
 }
