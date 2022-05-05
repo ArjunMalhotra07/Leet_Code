c2210e3bb57c644db5cd9004402c2c5850df45a3
ArjunMalhotra07 <arjun03.malhotra@gmail.com>
2022-05-06 00:18:02 +0530

    Search Insert Position

---
+++ arrays/03SearchInsertPosition.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	f := fmt.Println
+	array := []int{1, 3, 5, 6}
+	f(searchInsert(array, 7))
+}
+
+func searchInsert(nums []int, target int) int {
+	for i := 0; i < len(nums); i++ {
+		if nums[i] == target {
+			return i
+		}
+		if i+1 < len(nums) {
+			if nums[i] < target && nums[i+1] > target {
+				return i + 1
+			}
+		}
+		if i+1 == len(nums) && nums[len(nums)-1] < target {
+			return len(nums)
+		}
+
+	}
+	return 0
+
+}
